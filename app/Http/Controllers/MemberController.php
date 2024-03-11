8b67f01b4fc0164c8345382844e73e2654e29497
B00147616 Dilnaz Iskandarova <B00147616@mytudublin.ie>
2024-03-11 15:17:20 +0000

    added jq file

---
+++ app/Http/Controllers/MemberController.php
@@ -0,0 +1,48 @@
+<?php
+namespace App\Http\Controllers;
+use App\Models\Member as Member;
+use Illuminate\Http\Request;
+class MemberController extends Controller
+{
+    public function __construct()
+    {
+        //
+    }
+    public function showAllMembers()
+    {
+        $members = Member::all();
+        return response()->json($members);
+    }
+    public function showOneMember($id)
+    {
+        return response()->json(Member::find($id));
+    }
+
+    public function showMemberBookings($id)
+    {
+        $bookings = Booking::all()->where('memberid',$id);
+        return response()->json($bookings);
+    }
+
+    public function create(Request $request)
+    {
+        $arr = $request->json()->all();
+        $member = new Member();
+        $member->forceFill($arr);
+        $member->save();
+        return response()->json($member, 201);
+    }
+
+    public function update($id, Request $request)
+    {
+        $Member = Member::findOrFail($id);
+        $Member->update($request->all());
+        return response()->json($Member, 200);
+    }
+
+    public function delete($id)
+    {
+        Member::findOrFail($id)->delete();
+        return response('Deleted Successfully', 200);
+    }
+}
